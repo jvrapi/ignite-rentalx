a079f6e74734695a056d9e57c6b49055b2ba3cde
jvrapi <joaooviitorr@hotmail.com>
2022-02-24 22:23:15 -0300

    Added tsyringe; Refactored create category

--- src/modules/cars/useCases/categories/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/categories/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from 'tsyringe';
 import { Category } from '../../../entities/Category';
 import { ICategoriesRepository } from '../../../repositories/ICategoriesRepository';
 
@@ -6,8 +7,12 @@ interface IRequest {
     description: string;
 }
 
+@injectable() // diz que essa classe poderá ser injetada em outros locais
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject('CategoriesRepository') // injeta o repositorio que foi configurado no arquivo do index da pasta container
+        private categoriesRepository: ICategoriesRepository
+    ) {}
 
     async execute({ name, description }: IRequest): Promise<Category> {
         const categoryAlreadyExists =
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { createCategory } from '../modules/cars/useCases/categories/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/categories/createCategory/CreateCategoryController';
 import { importCategory } from '../modules/cars/useCases/categories/importCategory';
 import { listCategories } from '../modules/cars/useCases/categories/listCategories';
 
@@ -10,7 +10,9 @@ const upload = multer({
     dest: './tmp'
 });
 
-categoriesRoutes.post('/', createCategory);
+const createCategoryController = new CreateCategoryController();
+
+categoriesRoutes.post('/', createCategoryController.handle);
 
 categoriesRoutes.get('/', listCategories);
 
