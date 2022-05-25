e71374accf64e54d283de2ed1814cd650d1312e2
João Vitor <rapi@pop-os.localdomain>
2022-05-25 19:46:32 -0300

    added categories import and create specifications routes in swagger

--- src/modules/cars/useCases/categories/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/categories/importCategory/ImportCategoryController.ts
@@ -9,7 +9,7 @@ class ImportCategoryController {
         const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
         await importCategoryUseCase.execute(file);
-        return response.json();
+        return response.status(201).send();
     }
 }
 export { ImportCategoryController };
