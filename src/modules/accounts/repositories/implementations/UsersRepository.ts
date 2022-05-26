ecfde213cbad251fdb08250ae4277af611b82c3c
jvrapi <joaooviitorr@hotmail.com>
2022-05-26 19:36:12 -0300

    add users module

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUSerDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+    private repository: Repository<User>;
+
+    constructor() {
+        this.repository = getRepository(User);
+    }
+
+    async create({
+        driver_license,
+        email,
+        name,
+        password
+    }: ICreateUserDTO): Promise<void> {
+        const user = this.repository.create({
+            driver_license,
+            email,
+            name,
+            password
+        });
+        await this.repository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        return this.repository.findOne({ where: { email } });
+    }
+}
+
+export { UsersRepository };
---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+import { ICreateUserDTO } from '../../dtos/ICreateUSerDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({
+        name,
+        driver_license,
+        email,
+        password
+    }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error('User already exists.');
+        }
+
+        const passwordHash = await hash(password, 8);
+
+        await this.usersRepository.create({
+            driver_license,
+            email,
+            name,
+            password: passwordHash
+        });
+    }
+}
+
+export { CreateUserUseCase };
