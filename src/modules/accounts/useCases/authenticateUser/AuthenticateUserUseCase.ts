6a618914d64d6feda95050cd97763186579095a3
jvrapi <joaooviitorr@hotmail.com>
2022-05-26 19:52:51 -0300

    add authentication

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Email or password incorrect');
+        }
+
+        const passwordIsCorrect = await compare(password, user.password);
+
+        if (!passwordIsCorrect) {
+            throw new Error('Email or password incorrect');
+        }
+
+        const token = sign({}, '94a9a6c9764257f542ee9ede1db94c0e', {
+            subject: user.id,
+            expiresIn: '1d'
+        });
+
+        return {
+            user: {
+                name: user.name,
+                email: user.email
+            },
+            token
+        };
+    }
+}
+export { AuthenticateUserUseCase };
