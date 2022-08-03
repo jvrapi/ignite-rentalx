051e947ea253e2833076dc849d758c0226f4ee01
Joao Vitor <joao.lima@somoseducacao.com.br>
2022-08-03 10:21:50 -0300

    added list rentals by user

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -30,5 +30,14 @@ class RentalsRepository implements IRentalsRepository {
   findById(id: string): Promise<Rental> {
     return this.repository.findOne(id);
   }
+
+  findByUser(user_id: string): Promise<Rental[]> {
+    return this.repository.find({
+      where: {
+        user_id
+      },
+      relations: ['car']
+    });
+  }
 }
 export { RentalsRepository };
